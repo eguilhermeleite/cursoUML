b01e64ee7a12a2750dc0e6a3c87b53d2e424b25f
Edvaldo Leite <eguilhermeleite@gmail.com>
2020-04-24 00:35:21 -0300

    Projeto Criado

---
+++ src/test/java/com/edvaldoleite/cursoUML/CursoUmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edvaldoleite.cursoUML;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoUmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
