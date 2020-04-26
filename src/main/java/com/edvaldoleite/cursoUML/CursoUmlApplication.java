6ad162e85799aaf2c7d51aecad0876beb6a229df
Edvaldo Leite <eguilhermeleite>
2020-04-26 10:49:34 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/edvaldoleite/cursoUML/CursoUmlApplication.java
+++ src/main/java/com/edvaldoleite/cursoUML/CursoUmlApplication.java
@@ -8,13 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.edvaldoleite.cursoUML.domain.Categoria;
+import com.edvaldoleite.cursoUML.domain.Produto;
 import com.edvaldoleite.cursoUML.repositories.CategoriaRepository;
+import com.edvaldoleite.cursoUML.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoUmlApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoUmlApplication.class, args);
@@ -26,7 +32,20 @@ public class CursoUmlApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+	
 		
 	}
 
