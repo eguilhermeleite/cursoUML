d3199230db78e392a9180996a1ee10da848fe60c
Edvaldo Leite <eguilhermeleite>
2020-04-26 09:52:08 -0300

    Criando operação de Instanciação

--- src/main/java/com/edvaldoleite/cursoUML/CursoUmlApplication.java
+++ src/main/java/com/edvaldoleite/cursoUML/CursoUmlApplication.java
@@ -1,13 +1,33 @@
 package com.edvaldoleite.cursoUML;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.edvaldoleite.cursoUML.domain.Categoria;
+import com.edvaldoleite.cursoUML.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoUmlApplication {
+public class CursoUmlApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoUmlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
